052aa1b32c44a725bc2f77e4c0ce9b1417de3736
jbfeldman <yohan.feldman@gmail.com>
2019-03-31 16:50:36 -0400

    added initial server communication with router

--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h> 
 #include <sys/select.h>
 
 #include <sys/time.h>
@@ -12,10 +13,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define HEADER_LENGTH 70
+#define HEADER_LENGTH 66
 #define DISCONNECT_CODE -69
-#define MAX_NUM_CLIENTS 200
 #define MINUTE 60000
+#define NEW_SERVER_CODE 66
+#define NEW_SERVER_ACK_CODE 67
+#define ERROR_SERVER_EXISTS_CODE 68
 #define UPLOAD_FILE_CODE 256
 #define UPLOAD_ACK_CODE 257
 #define ERROR_FILE_EXISTS_CODE 258
@@ -26,13 +29,13 @@
 #define UPDATE_ACK_CODE 263
 #define ERROR_FILE_DOES_NOT_EXIST_CODE 264
 
+//TODO: set timeout value on read
 
 //TODO: if you eliminate a timed-out partial for upload, make sure to delete file that was being written 
 //linked list of partial messages
 struct PartialNode{
     int bytesRead;
-    char header[70];
-    char body[256];
+    char header[HEADER_LENGTH];
     int sockfd;
     unsigned int lastUpdated;
     struct PartialNode *next;
@@ -45,7 +48,7 @@ struct __attribute__((__packed__)) header {
     char user[20];
     char pwd[20];
     char fname[20];
-    unsigned long len;
+    int len;
 };
 
 void error(const char *msg)
@@ -54,7 +57,17 @@ void error(const char *msg)
     exit(1);
 } 
 
+//returns an allocated header
+struct header *makeHeader(int msgType, char *user, char * pwd, char* fname, int len){
+    struct header *myHeader = malloc(sizeof(struct header));
+    myHeader->msgType = msgType;
+    memcpy(myHeader->user, user, 20);
+    memcpy(myHeader->pwd, pwd, 20);
+    memcpy(myHeader->fname, fname, 20);
+    myHeader->len = len;
+    return myHeader;
 
+}
 //sets up the server sockets and binds it to the port
 int intializeLSock(int portno){
 
@@ -84,7 +97,6 @@ struct PartialNode * createPartial(int sockfd, struct PartialNode ** head, char 
     newHead->sockfd = sockfd;
     newHead->bytesRead = n;
     newHead->lastUpdated = time(NULL);
-    bzero(newHead->body, 256);
     bzero(newHead->header, HEADER_LENGTH);
     memcpy(newHead->header, buffer, n);
     newHead->next = *head;
@@ -108,6 +120,37 @@ struct PartialNode *findPartial(int sockfd, struct PartialNode * head){
         temp = temp->next;
     }
     return NULL;
+
+
+}
+
+//reads in file
+//writes ack to file
+//writes ack if successfull, error if not
+//returns DISCONNECT_CODE on succesfull read OR Error, returns 0 on partial read
+char uploadFile(int sockfd, struct PartialNode * node, struct PartialNode ** head){
+    //TODO: if file exists, send ERROR message return DISCONNECT CODE
+    struct header *msgHeader = (void *)node->header;
+    //TODO: verify filename has no special characteres except "."
+    char buffer[256];
+    int n;
+    long length = msgHeader->len;
+    FILE *fp = fopen(msgHeader->fname, "a");
+
+    //Read Loop
+    while(length > 0){
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 256);
+        if (n < 0){
+
+            //TODO: prepend the file with a special character so we know it isn't "ready"
+            return 0;
+        }
+        fwrite(buffer, 1, n, fp);
+        length -= n;
+    }
+    fclose(fp);
+    return DISCONNECT_CODE;
 
 
 }
@@ -139,6 +182,7 @@ int handleRequest(int sockfd, struct PartialNode ** head){
         return 0;
     }
     struct header* msgHeader = (void *) node->header;
+    fprintf(stderr, "the type of message incoming is %d\n", msgHeader->msgType);
 
     /* TODO: Deal with endianess */
     
@@ -146,6 +190,7 @@ int handleRequest(int sockfd, struct PartialNode ** head){
     switch(msgHeader->msgType){
         case UPLOAD_FILE_CODE:
             if (did_read != 1){
+              //  return uploadFile(sockfd, node, head);
                 //verify creds from SQL database, verify file doesn't exist, read, write file to Disk, send ACK
             }
             
@@ -160,9 +205,63 @@ int handleRequest(int sockfd, struct PartialNode ** head){
             }
             break;
             //TODO in future: add in permision cases
+            //TODO: Delete file
+            //TODO: 
     }
+    return DISCONNECT_CODE;
+
+}
+
+//connects to router, recieves ack, and returns socked number of connection with router
+//TODO: put the first half of this in a "connectToServer" function, possibly in a different file
+int connectToRouter(char *domainName, int portno, char* serverName){
+    char buffer[HEADER_LENGTH];
+    bzero(buffer, HEADER_LENGTH);
+
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+   
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(domainName);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
 
 
+    /* send message*/
+    char empty[20];
+    bzero(empty, 20);
+
+    struct header *headerBuf = makeHeader(NEW_SERVER_CODE, empty, empty, serverName, 0);
+
+
+    int n = write(sockfd, (void *)headerBuf, HEADER_LENGTH);
+    free(headerBuf);
+    bzero(headerBuf, HEADER_LENGTH);
+    n = read(sockfd, buffer, HEADER_LENGTH);
+    //POSSIBE TODO: this will fail if only one byte is read on a partial read. Extremely unlikely, but could happen
+    headerBuf = (void *) buffer;
+    if (headerBuf->msgType == NEW_SERVER_ACK_CODE){
+        return sockfd;
+    }
+    else if (headerBuf->msgType == ERROR_SERVER_EXISTS_CODE){
+        error("inputted servername already exists");
+    }
+    else{
+        error("unknown error connecting router, please try again");
+    }
+    return sockfd;
 
 
 }
@@ -177,7 +276,7 @@ int handleRequest(int sockfd, struct PartialNode ** head){
 int main(int argc, char *argv[])
 {
 
-    if (argc < 2) {
+    if (argc < 5) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
@@ -189,6 +288,7 @@ int main(int argc, char *argv[])
     clilen = sizeof(cli_addr);
 
     /* TODO: Talk with the router to say that you've come online */
+    //int routerSock = connectToRouter(argv[2], atoi(argv[3]), argv[1])
 
     int maxSock, rv, newSock = -1;
     struct timeval tv;
@@ -226,6 +326,8 @@ int main(int argc, char *argv[])
                         
                     }
                     else{
+                        //if sockfd = routerSock
+                            // addClient
                         int status = handleRequest(sockfd, &head);
 
                         if (status == DISCONNECT_CODE){
